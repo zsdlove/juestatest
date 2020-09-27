001af147e8af20c1e2f79a6bef36a18bc2ac9baf
zsdlove <2096350083@qq.com>
2020-09-27 22:58:24 +0800

    test

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+public class test {
+
+
+    void say(){
+        System.out.println("hello world");
+    }
+
+    public static void main(){
+
+        test t=new test();
+        t.say();
+    }
+}
