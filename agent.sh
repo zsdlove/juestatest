ab221abd42e19a9c8f3f263b511723e5e5f0af3b
zsdlove <2096350083@qq.com>
2020-09-28 11:30:40 +0800

    update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,7 +1,7 @@
 echo "======开始进行数据库构建======="
 echo "by pony@lixiang.com"
 echo "======agent环境检测=========="
-codeql database create dbfile --language=java'
+codeql database create dbfile --language=java
 zip -r dbfile.zip ./dbfile/*
 curl --form "file=@/Users/pony/.jenkins/workspace/test01_master/dbfile.zip" http://127.0.0.1:8088/fileupload/
 #curl --form "file=@/Users/pony/.jenkins/workspace/test01_master/target/mypiplinetest-1.0-SNAPSHOT.jar" http://127.0.0.1:8088/fileupload/'
